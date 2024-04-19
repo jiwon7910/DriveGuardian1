90371cb327baf20944239176f55f9d73745c3305
aajinlee <leehahj77@gmail.com>
2024-04-19 16:40:26 +0900

    Update HomeFragment.kt

--- app/src/main/java/com/example/poseexercise/views/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/poseexercise/views/fragment/HomeFragment.kt
@@ -47,10 +47,10 @@ class HomeFragment : Fragment(), /*PlanAdapter.ItemListener,*/ MemoryManagement 
     private var planList: List<Plan>? = emptyList()
     private var notCompletePlanList: MutableList<Plan>? = Collections.emptyList()
     // private var today: String = DateFormat.format("EEEE", Date()) as String
-    private lateinit var progressText: TextView
+    // private lateinit var progressText: TextView
     private lateinit var recyclerView: RecyclerView
     private lateinit var noPlanTV: TextView
-    private lateinit var progressBar: ProgressBar
+    // private lateinit var progressBar: ProgressBar
     private lateinit var progressPercentage: TextView
     private var workoutResults: List<WorkoutResult>? = null
     private lateinit var appRepository: AppRepository
@@ -75,8 +75,8 @@ class HomeFragment : Fragment(), /*PlanAdapter.ItemListener,*/ MemoryManagement 
         // recentActivityRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         // recentActivityRecyclerView.adapter = recentActivityAdapter
         // noPlanTV = view.findViewById(R.id.no_plan)
-        progressBar = view.findViewById(R.id.progress_bar)
-        progressPercentage = view.findViewById(R.id.progress_text)
+        // progressBar = view.findViewById(R.id.progress_bar)
+        // progressPercentage = view.findViewById(R.id.progress_text)
         appRepository = AppRepository(requireActivity().application)
         // Initialize ViewModel
         resultViewModel = ResultViewModel(MyApplication.getInstance())
@@ -189,7 +189,7 @@ class HomeFragment : Fragment(), /*PlanAdapter.ItemListener,*/ MemoryManagement 
              */
         }
     }
-
+/*
     // Function to update progress views (ProgressBar and TextView)
     private fun updateProgressViews(progress: Int) {
         // Check if progressPercentage is greater than 0
@@ -204,6 +204,8 @@ class HomeFragment : Fragment(), /*PlanAdapter.ItemListener,*/ MemoryManagement 
             progressText.visibility = View.GONE
         }
     }
+
+ */
 
     // Return true if the timestamp is today's date
     private fun isToday(s: Long, locale: Locale = Locale.getDefault()): Boolean {
